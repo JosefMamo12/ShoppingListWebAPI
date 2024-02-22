e6e8fa5375e919b5f0293fdd4b7cb70b415db68d
JosefTheKing17 <yosimami50@gmail.com>
2024-02-22 07:05:19 +0200

    for work

--- ShoppingListWebAPI.Server/Models/Category.cs
+++ ShoppingListWebAPI.Server/Models/Category.cs
@@ -10,5 +10,8 @@ namespace ShoppingListWebAPI.Server.Models
         public string Name { get; set; } = null!;
         public int CategoryQuantity { get; set; }
         public ICollection<Product> Products { get; } = new List<Product>();
+
+        public void AddProduct (Product product) => Products.Add(product);
+
     }
 }
