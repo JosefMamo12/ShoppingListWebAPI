4daba1a5110a66de50f55b6e332d01570472219c
JosefTheKing12 <yosimami50@gmail.com>
2024-03-05 10:26:05 +0200

    TODO: Add Mapper and Profiler for the server rearange dtos, and Data folders

---
+++ ShoppingListWebAPI.Server/Data/ICategoryRepo.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingListWebAPI.Server.Models;
+
+namespace ShoppingListWebAPI.Server.Data
+{
+    public interface ICategoryRepo: IShoppingListRepo<Category>
+    {
+        IEnumerable<Category> GetAllCategories();
+        IEnumerable<Category> GetCategoriesProducts();
+    }
+}
---
+++ ShoppingListWebAPI.Server/Data/IShoppingListRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingListWebAPI.Server.Data
+{
+    public interface IShoppingListRepo<T>
+    {
+        bool SaveChanges();
+        void Add(T entitity);
+        void Update(T entitity);
+        T get(int id);
+        void delete(int id);
+    }
+}
